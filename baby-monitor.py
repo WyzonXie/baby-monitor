035cd597e9483fa6b8fc9cc0f2711f001ed73627
WyzonXie <xiewyzon@gmail.com>
2026-08-05 17:30:32 +0800

    feat: volume threshold detection

--- baby-monitor.py
+++ baby-monitor.py
@@ -1,8 +1,12 @@
 import sounddevice as sd
 SAMPLE_RATE = 16000
 SECONDS = 1
+THRESHOLD = 0.2  #silent ~0.03 speaking ~0.3
 print("Monitoring...")
 while True:
     audio_data = sd.rec(SAMPLE_RATE * SECONDS, samplerate=SAMPLE_RATE, channels=1)
     sd.wait()
-    print(abs(audio_data).max())
+    volume = abs(audio_data).max()
+    print(volume)
+    if volume > THRESHOLD:
+        print("Sound detected!")
