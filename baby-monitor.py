db193fb5a48ca2ebc6b0de730e885b420c165c41
WyzonXie <xiewyzon@gmail.com>
2026-08-05 17:22:38 +0800

    feat: baby monitor main loop

---
+++ baby-monitor.py
@@ -0,0 +1,8 @@
+import sounddevice as sd
+SAMPLE_RATE = 16000
+SECONDS = 1
+print("Monitoring...")
+while True:
+    audio_data = sd.rec(SAMPLE_RATE * SECONDS, samplerate=SAMPLE_RATE, channels=1)
+    sd.wait()
+    print(abs(audio_data).max())
